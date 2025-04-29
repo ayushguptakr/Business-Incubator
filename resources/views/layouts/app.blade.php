3fc674d514a89ce7632872ffd433a1af69cd9296
ayushguptakr144@gmail.com <ayushguptakr144@gmail.com>
2025-04-29 18:53:25 +0530

    Initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,12 +89,7 @@
         </div>
     </nav>
 
-    @if(session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-    </div>
-@endif
+
 
 
     {{-- Hero Section - Only shown on home page --}}
